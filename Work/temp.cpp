28a420a60e4c1e87937a170cf2245fac591d3973
Tahsin000 <59111015+Tahsin000@users.noreply.github.com>
2021-07-14 13:04:33 +0600

    14/07/2021

--- Work/temp.cpp
+++ Work/temp.cpp
@@ -1,160 +1,66 @@
 /*
     بسم الله الرحمن الرحيم
-
      الجامعة الإسلامية العالمية شيتاغونغ
-
     لا تحزن ان الله معنا
-
 ///////////// IIUC - 49 /////////////
-
-    PROBLEM :
-*/
-
-
+*/ 
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
 #define MX 100000000
 #define fr(i, a, b) for (ll i=a; i<b; i++)
 #define rfr(i, a, b) for (ll i=a; i>=b; i--)
-#define dev(x) cout << #x << " : " << x << endl;
+#define ts(x) cout << #x << " : " << x << endl;
 #define HHJN ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define pera freopen("inputf.in","r", stdin);freopen("outputf.in","w", stdout)
 #define test ll t; cin >> t; while(t--){solve();};
-
 #define yes cout << "YES\n"
 #define no cout << "NO\n"
-
 #define all(n) n.begin(), n.end()
 #define uper(n) transform(n.begin(), n.end(), n.begin(), ::toupper);
 #define lower(n) transform(n.begin(), n.end(), n.begin(), ::tolower);
-
 #define vl(n) std::vector<ll> v(n+10);
 #define pb push_back
 #define sz(n) n.size()
 
-// ll arr[mx]= {0}, prime[mx];
-int binarySearch(int arr[], int l, int r, int x);
-ll gcd(ll a, ll b);
-ll lcm(ll a, ll b);   
-ll BigMod(ll b, ll p, ll m);
-void sieve (vector<ll>&v);
-string to_string(ll n);
-ll to_int(string s);
+ll gcd(ll a, ll b) {if (b==0) return a;return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return (a / gcd(a, b)) * b;}
+string to_String(ll n) {string s;while(n){s.pb(n%10+'0');n/=10;}reverse(all(s));return s; }
+ll to_int(string s) {ll n = 0, i =0, j = 1;fr(i, 0, s.size()){n += (s[s.size()-(i+1)] - '0') * j; j*=10;}return n; }
+ll BigMod(ll b, ll p, ll m) {if (p == 0) return 1;ll ans = BigMod(b, p/2, m);ans = ( ans * ans ) % m;if (p % 2 == 1) ans = (ans * b) % m;return ans;}
+int binarySearch(int arr[], int l, int r, int x) {if (r >= l){int mid = l+(r-l) / 2;if (arr[mid] == x) return mid;if (arr[mid] > x) return binarySearch(arr, l, mid-1, x);return binarySearch(arr, mid+1, r, x);}return -1 ;}
 
-/////////////////////////////////////////////////////
-
+/* Start Your FUN*/
+ll fun(ll n)
+{
+    
+}
+/* Start Your CODE*/
 void solve()
 {
-    ll n;
-    cin >> n;
-    std::vector<ll> v;
-    fr(i, 0, n)
-    {
-        ll m;
-        cin >> m;
-        v.pb(m);
-    }
-    
-    ll cont = 0;
-    fr(i, 0, sz(v)-1)
-    {
-        fr(j, i+1, sz(v))
-        {
-            if (gcd(v[i], 2*v[j]) > 1 || gcd(v[j], 2*v[i]) > 1)
-                cont++;
-        }
-    }
-    cout << cont << endl;
-
-}
-
-/////////////////////////////////////////////////////
-
-void fun()
-{
+     
     
 }
-
+/* Start Your MAIN*/
 int main()
 {
+    // pera;
     HHJN;
-    test
-    return 0;
+    
+    // test
     solve();
+    return 0;
+
 }
-
-ll gcd(ll a, ll b)
-{
-    if (b==0) return a;
-    return gcd(b, a%b);
-}
-
-ll BigMod(ll b, ll p, ll m)
-{
-    if (p == 0) return 1;
-
-    ll ans = BigMod(b, p/2, m);
-    ans = ( ans * ans ) % m;
-    if (p % 2 == 1) ans = (ans * b) % m;
-    return ans;
-}
-
-int binarySearch(int arr[], int l, int r, int x)
-{
-    if (r >= l)
-    {
-        int mid = l+(r-l) / 2;
-        if (arr[mid] == x) return mid;
-        if (arr[mid] > x) return binarySearch(arr, l, mid-1, x);
-        return binarySearch(arr, mid+1, r, x);
-    }
-    return -1 ;
-}
-
 void sieve (vector<ll>&v)
-{
-    v.assign(MX, 0);
-    v[0] = 1;
-    v[1] = 1;
+{   v.assign(MX, 0);
+    v[0] = v[1] = 1;
     for (int i=4; i<=MX; i+=2) v[i] = 1;
-
-    for (int i=3; i*i<=MX; i+=2)
-    {
-        if (v[i] == 0)
-        {
+    for (int i=3; i*i<=MX; i+=2){
+        if (v[i] == 0){
             for (int j=i*i; j<=MX; j+= i*2)
-            {
                 v[j] = 1;
-            }
         }
     }
-
 }
 
-ll lcm(ll a, ll b)
-{
-    return (a / gcd(a, b)) * b;
-}
-
-string to_string(ll n)
-{
-    string s;
-    while(n)
-    {
-        s.pb(n%10+'0');
-        n/=10;
-    }
-    reverse(all(s));
-    return s;
-}
-
-ll to_int(string s)
-{
-    ll n = 0, i =0, j = 1;
-    fr(i, 0, s.size())
-    {
-        n += (s[s.size()-(i+1)] - '0') * j;
-        j*=10;
-    }
-    return n;
-}
