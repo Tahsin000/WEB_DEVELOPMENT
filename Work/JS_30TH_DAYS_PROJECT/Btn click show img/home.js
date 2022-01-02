43f50671736c35862281ad5adc61eb55c56de14c
Tahsin000 <59111015+Tahsin000@users.noreply.github.com>
2022-01-02 20:18:47 +0600

    02.01.2021

--- Work/JS_30TH_DAYS_PROJECT/Btn click show img/home.js
+++
@@ -1,9 +0,0 @@
-var IM = document.getElementById('I');
-        function img1()
-        {
-            IM.src = "https://images.unsplash.com/photo-1597589827317-4c6d6e0a90bd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80";
-        }
-        function img2()
-        {
-            IM.src = "https://images.unsplash.com/photo-1495615080073-6b89c9839ce0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=522&q=80";
-        }
