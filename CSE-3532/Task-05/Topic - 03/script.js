29d990d627da4cfb05a04f392cb81b9f89226af1
Tahsin000 <59111015+Tahsin000@users.noreply.github.com>
2022-03-23 11:22:36 +0600

    23.03.2022

---
+++ CSE-3532/Task-05/Topic - 03/script.js
@@ -0,0 +1,17 @@
+function Nclick(){
+    var n1 = Number(document.getElementById('firstID').value);
+    var n2 = Number(document.getElementById('secondID').value);
+    console.log(n1);
+    // alert(n1);
+    var addition = Number(n1 + n2);
+    var minus = Number(n1 - n2);
+    var multiplication = Number(n1 * n2);
+    var division = Number(n1 / n2);
+    
+    document.getElementById('addition').innerHTML = Number(n1 + n2);
+    document.getElementById('minus').innerHTML = minus;
+    document.getElementById('multiplication').innerHTML = multiplication;
+    document.getElementById('division').innerHTML = division;
+    document.getElementById('power').innerHTML = Math.pow(n1, n2);
+    document.getElementById('sqrt').innerHTML = Math.sqrt(n1, n2);
+}
