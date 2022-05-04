4d0b3075b4c512f94a2927eeacda41ea2d5b644e
Tahsin000 <59111015+Tahsin000@users.noreply.github.com>
2022-05-04 23:50:20 +0600

    09/04/22

--- CSE-3532/Task-06/index.php
+++ CSE-3532/Task-06/index.php
@@ -12,47 +12,62 @@
     for($i=0;$i<count($Test);$i++){
         echo $Test[$i]."<br>";
     }
-    echo '<br>**************** String Len ******************<br><br>';
+    echo '<br><br>**************** String Len ******************<br>';
  
     $STR = "Tahsin Abrar";
+    echo "String is = Tahsin Abrar<br>String Len";
     echo strlen($STR); // outputs 12
  
-    echo '<br>**************** INT CHECK ******************<br><br>';
+    echo '<br><br>**************** INT CHECK ******************<br>';
+    echo "Number is = 59.85 <br>";
     $x = 59.85;
     var_dump(is_int($x));
  
-    echo '<br>**************** Php math ******************<br><br>';
-    echo '<br>***** PI VALUE ***** <br>';
+    echo '<br><br>**************** PHP math ******************<br>';
+    echo "<br><b>PI VALUE</b><br>";
+    echo "PI Value is = 3.1416<br> And PHP.Math Values is = ";
     echo(pi());
-    echo '<br>***** MAX VALUE ***** <br>';
+    echo '<br> <br> <b>MIN VALUE CHECK</b><br>';
     echo(min(0, 150, 30, 20, -8, -200));  // returns -200
  
     //***** function declaration *****
+    echo "<br><br>**************** function declaration *****************<br>";
     function myTest() {
         static $x = 0;
         echo $x;
         $x++;
     }
-        myTest();
-        myTest();
-        myTest();
+        myTest(); echo "<br>";
+        myTest(); echo "<br>";
+        myTest(); echo "<br>";
 
     //***** Reverse string *****
+    echo "<br><br>**************** Reverse string *****************<br> My String is = Hello world!<br>Reverse is = ";
     echo strrev("Hello world!"); // outputs !dlrow olleH
-
+    
     //***** Replace string *****
+    echo "<br><br>**************** Replace string *****************<br>My string is = Hello world!<br> Replace 'world' to 'Dolly'";
     echo str_replace("world", "Dolly", "Hello world!"); // outputs Hello Dolly!
 
     //***** generates a random number *****
+    echo "<br><br>**************** generates a random number *****************<br>";
     echo(rand());
+        echo "<br><br><b>Random number 10 to 100</b> <br>";
         // Range of random number
         echo(rand(10, 100));
 
     //***** Define(case-sensitive name) *****
+    echo "<br><br>**************** Define(case-sensitive name) *****************<br>";
+    echo "My Variable 'GREETING' which is assigned a string 'Welcome to W3Schools.com!'<br>";
     define("GREETING", "Welcome to W3Schools.com!");
     echo GREETING;
-        // case-insensitive
-        define("GREETING", "Welcome to W3Schools.com!", true);
-        echo greeting;
 
+    echo "<br><br>**************** PHP Date  *****************<br>";
+    echo "Today is ".date("d / m/ y")." ".date("l");
+
+    echo "<br><br>**************** PHP Time *****************<br>";
+    echo "Today is ".date("h:i:sa");
+
+    echo "<br><br>**************** PHP include *****************<br>";
+    echo "Copyright &copy; 1999-" . date("Y") . " hotash.com";
 ?>
